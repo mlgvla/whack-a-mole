56a0ab8bbbd10163cc4b042b03f9af070a7f0ba0
Monica Gerard <mlgvla@gmail.com>
2023-04-28 11:56:43 -0400

    completes solution with advanced deliverables

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 
 // 1. get ALL the divs with class "hole" (Replace quotes with a document query method)
 
-const holes = ""
+const holes = document.querySelectorAll(".hole")
 
 // 2. get ALL the divs with class "mole"
 
-const moles = ""
+const moles = document.querySelectorAll(".mole")
 
 // 3. get the button with the id "start-btn"
 
-const startBtn = ""
+const startBtn = document.querySelector("#start-btn")
 
 // 4. get the scoreboard - the div with the class "score"
 
-const scoreboard = ""
+const scoreboard = document.querySelector(".score")
 
 
 // Variables
@@ -26,10 +26,10 @@ let score = 0
 // Event Listeners
 
 // 5. attach a click event listener to the startBtn that uses the  callback function that starts the game
-
+startBtn.addEventListener("click", startGame)
 
 // 6. attach a click event to EACH mole that uses the callback function that whacks a mole
-
+moles.forEach(mole => mole.addEventListener("click", whack))
 
 // Functions
 
@@ -60,14 +60,16 @@ function peep() {
 // Callback Functions
 
 function startGame() {
+   let gameLength = document.querySelector("#game-length").value //string - should convert to int
+   gameLength = parseInt(gameLength)
    scoreboard.textContent = 0
    timeUp = false
    score = 0
    // 7. what function needs to be called so the moles can start to appear?
-   // ??
+   peep()
    setTimeout(() => {
       timeUp = true
-   }, 10000)
+   }, gameLength)
 }
 
 function whack(e) {
@@ -83,14 +85,22 @@ function whack(e) {
 
 // 1. Get all the radio buttons by the name attribute
 
-const radioBtns = ""
+const radioBtns = document.getElementsByName("image")
 
 // 2. For each radio button, add an event listener that uses the "change event"
    // The event handler should use the event's target value property to determine which image
    // to invoke the changeImage function with
 
-/// YOUR CODE HERE
+console.log(radioBtns)
 
+/// YOUR CODE HERE
+radioBtns.forEach(radioBtn => radioBtn.addEventListener("change", (e) => {
+   if (e.target.value === "mole-image") {
+      changeImage("./images/mole.svg")
+   } else {
+      changeImage("./images/tom.png")
+   }
+ }))
 
 
 function changeImage(imageURL) {
